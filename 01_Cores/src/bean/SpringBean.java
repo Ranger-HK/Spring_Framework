46b51b2d61ffda1b47dcc859a937de06dd9eb685
Ranger-HK <maxravindu99@gmail.com>
2024-05-03 16:37:28 +0530

    Check Spring Work

---
+++ 01_Cores/src/bean/SpringBean.java
@@ -0,0 +1,15 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 5/3/2024 - 4:29 PM
+ * @project Spring_Framework
+ */
+@Component
+public class SpringBean {
+    public SpringBean() {
+        System.out.println("Spring Bean Object Created");
+    }
+}
---
+++ 01_Cores/src/config/AppConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 5/3/2024 - 4:29 PM
+ * @project Spring_Framework
+ */
+@Configuration
+@ComponentScan(basePackages = "bean")
+public class AppConfig {
+}
