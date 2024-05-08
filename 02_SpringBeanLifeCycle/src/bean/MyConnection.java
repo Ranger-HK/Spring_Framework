6d38928a06b842292e239563e192558572bba0a8
Ranger-HK <maxravindu99@gmail.com>
2024-05-08 12:27:19 +0530

    Updated

--- 02_SpringBeanLifeCycle/src/bean/MyConnection.java
+++ 02_SpringBeanLifeCycle/src/bean/MyConnection.java
@@ -2,13 +2,13 @@ package bean;
 
 import org.springframework.beans.factory.DisposableBean;
 
-public class MyConnection {
+public class MyConnection implements DisposableBean{
     public MyConnection() {
-        System.out.println("My Connection");
+        System.out.println("My Connec tion");
     }
 
- /*   @Override
+    @Override
     public void destroy() throws Exception {
         System.out.println("My Connection Destroyed...");
-    }*/
+    }
 }
