afcae8c0da9d35611be17d708061069d3d4ec9ba
Ranger-HK <maxravindu99@gmail.com>
2024-05-09 16:06:15 +0530

    Updated

---
+++ 03_DI_with_Spring/src/bean/SpringBeanOne.java
@@ -0,0 +1,17 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 5/8/2024 - 10:54 AM
+ * @project Spring_Framework
+ */
+@Component
+public class SpringBeanOne {
+
+    public SpringBeanOne() {
+        System.out.println("Spring Bean One Instantiated");
+    }
+
+}
