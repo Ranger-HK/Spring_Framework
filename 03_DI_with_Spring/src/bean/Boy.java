eeb21866b03f89d3424b28c18082482ec3d934eb
Ranger-HK <maxravindu99@gmail.com>
2024-05-12 11:51:23 +0530

    Updated

--- 03_DI_with_Spring/src/bean/Boy.java
+++ 03_DI_with_Spring/src/bean/Boy.java
@@ -1,5 +1,6 @@
 package bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,12 +11,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Boy {
+
+    @Autowired
+    Girl girl;
+
     public Boy() {
         System.out.println("Boy Instantiated");
     }
 
     public void chattingWithGirl(){
-        Girl girl = new Girl();
         girl.chat();
     }
 }
