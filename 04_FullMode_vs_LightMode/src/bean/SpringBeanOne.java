b93fcfe087f7c1adf0eb38def0746ec60c05c1c6
Ranger-HK <maxravindu99@gmail.com>
2024-05-12 14:48:56 +0530

    Updated

---
+++ 04_FullMode_vs_LightMode/src/bean/SpringBeanOne.java
@@ -0,0 +1,15 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 5/12/2024 - 2:47 PM
+ * @project Spring_Framework
+ */
+@Component
+public class SpringBeanOne {
+    public SpringBeanOne() {
+        System.out.println("Spring Bean One");
+    }
+}
---
+++ 04_FullMode_vs_LightMode/src/config/AppConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 5/8/2024 - 10:53 AM
+ * @project Spring_Framework
+ */
+@Configuration
+@ComponentScan(basePackages = "bean")
+public class AppConfig {
+    public AppConfig() {
+        System.out.println("Create Config Object");
+    }
+
+
+}
