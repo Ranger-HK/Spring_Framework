90438ed683c7ed7590f476c6ea3b50c3248d7c5c
Ranger-HK <maxravindu99@gmail.com>
2024-05-14 22:56:30 +0530

    Updated

--- 06_RuntimeValueInjection/src/bean/SpringBeanOne.java
+++ 06_RuntimeValueInjection/src/bean/SpringBeanOne.java
@@ -16,7 +16,7 @@ public class SpringBeanOne {
         System.out.println(id);
     }*/
 
-    @Autowired(required = false)
+    @Autowired(required = false) // Greediest Constructor
     public SpringBeanOne(@Value("C001") String id , @Value("10") int age,@Value("true") boolean b) {
         System.out.println("Spring Bean One Instantiated");
         System.out.println(id);
@@ -25,7 +25,7 @@ public class SpringBeanOne {
     }
 
     @Autowired(required = false)
-    public SpringBeanOne(@Value("1,2,3") int[] myNames,@Value("A") char a) {
+    public SpringBeanOne(@Value("1,2,3") int[] myNames,@Value("A") char a,@Value("1,,3") int[] n,@Value("3,7") int[] n2) {
         System.out.println("Spring Bean One Instantiated");
         for (Integer myName : myNames){
             System.out.println(myName);
