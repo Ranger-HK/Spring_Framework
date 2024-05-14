62e0ed85666026f644ed82a1aff73a0450dd185f
Ranger-HK <maxravindu99@gmail.com>
2024-05-14 14:40:40 +0530

    Updated

---
+++ 05_Configurations/src/AppInitializer.java
@@ -0,0 +1,34 @@
+import config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 5/3/2024 - 4:29 PM
+ * @project Spring_Framework
+ */
+public class AppInitializer {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
