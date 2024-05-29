c1c92b3229b47fc53c4cc7d8d1aac7241b5e9d14
Ranger-HK <maxravindu99@gmail.com>
2024-05-29 10:33:25 +0530

    Updated

--- 07_SpringEnvironment/src/AppInitializer.java
+++ 07_SpringEnvironment/src/AppInitializer.java
@@ -1,6 +1,9 @@
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+import java.util.Properties;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 5/3/2024 - 4:29 PM
@@ -13,6 +16,22 @@ public class AppInitializer {
         ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
+
+
+        //Access System Environment Variables in Java
+        //System.getenv()  -- use that key
+        Map<String, String> getenv = System.getenv();
+        for (String key:getenv.keySet()) {
+            System.out.println(key+" : "+getenv.get(key));
+        }
+
+
+        //Access Java Properties Variables in Java
+        //System.getProperties(); -- use that key
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            System.out.println(key+" : "+properties.get(key));
+        }
 
     }
 
