6036ac2648c8655e436e2916e1073045f5e66d79
Ranger-HK <maxravindu99@gmail.com>
2024-06-03 13:42:46 +0530

    Sprin MVC - WebRoot and Bean Class Done

--- 08_Spring_MVC/src/bean/SpringBean.java
+++ 08_Spring_MVC/src/bean/SpringBean.java
@@ -1,9 +1,15 @@
 package bean;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 6/3/2024 - 1:27 PM
  * @project Spring_Framework
  */
+@Component
 public class SpringBean {
+    public SpringBean() {
+        System.out.println("Spring Bean One");
+    }
 }
--- 08_Spring_MVC/src/config/WebRootConfig.java
+++ 08_Spring_MVC/src/config/WebRootConfig.java
@@ -1,9 +1,12 @@
 package config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 6/3/2024 - 1:27 PM
  * @project Spring_Framework
  */
+@Configuration
 public class WebRootConfig {
 }
