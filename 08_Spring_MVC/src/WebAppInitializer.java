f008f2050d141cbfb5337e6e4cbf80e2827e6ad2
Ranger-HK <maxravindu99@gmail.com>
2024-06-03 13:37:39 +0530

    Sprin MVC - WebAppInitializer Class Create

--- 08_Spring_MVC/src/WebAppInitializer.java
+++ 08_Spring_MVC/src/WebAppInitializer.java
@@ -1,7 +1,25 @@
+import config.WebAppConfig;
+import config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 6/3/2024 - 1:26 PM
  * @project Spring_Framework
  */
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
