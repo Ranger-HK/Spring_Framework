c82323a81aaec85af014b46941bec1184cb140e2
Ranger-HK <maxravindu99@gmail.com>
2024-06-03 14:01:08 +0530

    Updated

--- 08_Spring_MVC/src/bean/SpringBean.java
+++ 08_Spring_MVC/src/bean/SpringBean.java
@@ -1,5 +1,9 @@
 package bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,8 +12,38 @@ import org.springframework.stereotype.Component;
  * @project Spring_Framework
  */
 @Component
-public class SpringBean {
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBean() {
         System.out.println("Spring Bean One");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Spring Bean One Factory ");
+
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Spring Bean One Awareness");
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Spring Bean One Destroy");
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Spring Bean One Initializing");
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Spring Bean One Application Context Aware Call");
+
     }
 }
