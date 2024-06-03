70cfafe9642ef19e347c3d624117b2c15c8b139d
Ranger-HK <maxravindu99@gmail.com>
2024-06-03 13:40:17 +0530

    Spring MVC - WebAppConfig Class Create

--- 08_Spring_MVC/src/config/WebAppConfig.java
+++ 08_Spring_MVC/src/config/WebAppConfig.java
@@ -1,9 +1,16 @@
 package config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 6/3/2024 - 1:27 PM
  * @project Spring_Framework
  */
+@Configuration
+@ComponentScan(basePackages = "bean")
+@EnableWebMvc
 public class WebAppConfig {
 }
