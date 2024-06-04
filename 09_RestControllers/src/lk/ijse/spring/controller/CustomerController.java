7823cefc97fdf203132cc229eda0b429af7ca0c9
Ranger-HK <maxravindu99@gmail.com>
2024-06-04 16:34:07 +0530

    Updated

--- 09_RestControllers/src/lk/ijse/spring/controller/CustomerController.java
+++ 09_RestControllers/src/lk/ijse/spring/controller/CustomerController.java
@@ -1,8 +1,6 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Created By Ravindu Prathibha
@@ -24,5 +22,15 @@ public class CustomerController {
     @PostMapping
     public String saveCustomer(){
         return "Customer Save and Post Method Invoked";
+    }
+
+    @DeleteMapping
+    public String deleteCustomer(){
+        return "Customer Delete and Delete Method Invoked";
+    }
+
+    @PutMapping
+    public String updateCustomer(){
+        return "Customer Update and Update Method Invoked";
     }
 }
