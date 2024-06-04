03709df617720b3ab807b4e102bf9c1454a91d95
Ranger-HK <maxravindu99@gmail.com>
2024-06-04 16:49:43 +0530

    Updated

---
+++ 09_RestControllers/src/lk/ijse/spring/controller/ItemController.java
@@ -0,0 +1,21 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 6/4/2024 - 4:45 PM
+ * @project Spring_Framework
+ */
+@RestController
+@RequestMapping("item")
+public class ItemController {
+
+    @GetMapping //Handler Mapping
+    //Handler Method
+    public String getAllItems(){
+        return "All Items Get and Get Method Invoked";
+    }
+
+
+}
