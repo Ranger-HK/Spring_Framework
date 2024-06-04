10ec4081a84c2d162e70a87c8f38cb5eeeeba1f5
Ranger-HK <maxravindu99@gmail.com>
2024-06-04 17:55:01 +0530

    Updated

---
+++ 09_RestControllers/src/lk/ijse/spring/controller/PathVariableController.java
@@ -0,0 +1,29 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 6/4/2024 - 5:44 PM
+ * @project Spring_Framework
+ */
+@RestController
+@RequestMapping("variable")
+public class PathVariableController {
+
+   /* http://localhost:8080/09_RestControllers_Web_exploded/variable/I001"
+    (variable) path segment
+    (I0001) path segment
+
+    In spring we can retrive values of path segments
+    To Do that we have to create path variables ->{1001}*/
+
+
+    @GetMapping(path = "{I001}")
+    public String test(@PathVariable("I001") String itemcode){
+        return itemcode;
+    }
+}
