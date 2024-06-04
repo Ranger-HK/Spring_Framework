6172ca828672159473d5a077127b97fb99ac165c
Ranger-HK <maxravindu99@gmail.com>
2024-06-04 15:30:59 +0530

    Updated

--- 08_Spring_MVC/src/config/WebAppConfig.java
+++ 08_Spring_MVC/src/config/WebAppConfig.java
@@ -1,8 +1,13 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * @Created By Ravindu Prathibha
@@ -13,5 +18,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //@ComponentScan(basePackages = {"bean","controller"})
 @ComponentScan(basePackages = "controller")
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+
+    //create viewResolver
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/views/");
+        internalResourceViewResolver.setSuffix(".html");
+        internalResourceViewResolver.setOrder(2);
+        return internalResourceViewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/views/**").addResourceLocations("views/");
+    }
 }
