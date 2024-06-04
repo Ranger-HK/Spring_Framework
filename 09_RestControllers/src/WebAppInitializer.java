7e361226ea5af35dcce09827daa1198193dc780f
Ranger-HK <maxravindu99@gmail.com>
2024-06-04 16:00:34 +0530

    Updated

---
+++ 09_RestControllers/src/WebAppInitializer.java
@@ -0,0 +1,25 @@
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 6/3/2024 - 1:26 PM
+ * @project Spring_Framework
+ */
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; //default mapping
+    }
+}
---
+++ 09_RestControllers/src/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 6/4/2024 - 3:52 PM
+ * @project Spring_Framework
+ */
+@RestController
+@RequestMapping("customer")
+public class CustomerController {
+    public CustomerController() {
+        System.out.println("Controller");
+    }
+
+    @RequestMapping
+    public String getAllCustomer(){
+        return "Hello Customer";
+    }
+}
