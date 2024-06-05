0c3b75007cf54ffeb0a167e2ea9c16a460044dd3
Ranger-HK <maxravindu99@gmail.com>
2024-06-05 15:34:38 +0530

    Updated

--- 09_RestControllers/src/lk/ijse/spring/controller/JSONController.java
+++ 09_RestControllers/src/lk/ijse/spring/controller/JSONController.java
@@ -4,6 +4,8 @@ import lk.ijse.spring.dto.CustomerDTO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 6/5/2024 - 2:05 PM
@@ -13,19 +15,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("json")
 public class JSONController {
 
-  /*  @PutMapping
+    @PutMapping
     public String getJsonRequest(){
       return "Hello Json";
     }
 
-    @PostMapping
-    public String getJsonRequest(CustomerDTO dto){
-      return "Hello Json"+dto.toString();
+    @PostMapping //if you want to get request content you have to use @RequestBody
+    public String getJsonRequest(@RequestBody CustomerDTO dto){
+      return "Hello Json hi"+dto.toString();
     }
-    */
 
+    //To return json response you need json converter in the classpath
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE}) //content-type=application/json
     public CustomerDTO sendBackJSON(){
       return new CustomerDTO("C001","Dasu","Colombo",100.00);
+    }
+
+
+    @GetMapping(path = "array",produces = {MediaType.APPLICATION_JSON_VALUE}) //content-type=application/json
+    public ArrayList<CustomerDTO> sendBackJSONArray(){
+        ArrayList<CustomerDTO> objects = new ArrayList<>();
+        objects.add(new CustomerDTO("C001","Dasu","Colombo",100.00));
+        objects.add(new CustomerDTO("C002","Ravi","Bandaragama",100.00));
+        objects.add(new CustomerDTO("C003","Kamal","Panadura",100.00));
+        return objects;
     }
 }
