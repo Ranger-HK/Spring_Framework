9fd24b959c38e99c4bfe6a16ad7e91ccf73e756c
Ranger-HK <maxravindu99@gmail.com>
2024-06-08 15:01:55 +0530

    updated

--- 10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customer")
 public class CustomerController {
 
-    @GetMapping
+   /* @GetMapping
     public String getAllCustomers(){
         return "Hello Maven";
+    }*/
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) //content-type=application/json
+    public CustomerDTO getAllCustomers() {
+        return new CustomerDTO("C001", "Ravindu", "Bandaragama", 100000.00);
     }
 }
