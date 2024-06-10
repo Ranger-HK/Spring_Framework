e762b52972221235f5cb0df47b3c1b01d4bacddc
Ranger-HK <maxravindu99@gmail.com>
2024-06-10 15:47:19 +0530

    updated

--- 10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,7 +1,12 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 /**
  * @Created By Ravindu Prathibha
@@ -10,5 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("api/v1/customer")
+@CrossOrigin
 public class CustomerController {
+
+    @GetMapping
+    public ArrayList<CustomerDTO>getAllCustomers(){
+        ArrayList<CustomerDTO> arrayList = new ArrayList<>();
+        arrayList.add(new CustomerDTO("C001","Ravindu","Bandaragama",100000));
+        arrayList.add(new CustomerDTO("C002","Kamal","Panadura",20000));
+        arrayList.add(new CustomerDTO("C003","Nimal","Colombo",30000));
+        return arrayList;
+    }
 }
