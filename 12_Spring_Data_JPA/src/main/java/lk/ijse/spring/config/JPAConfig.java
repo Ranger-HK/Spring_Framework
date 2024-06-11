6bd23d25da4e7906ff0e551a7008e1f870e88f98
Ranger-HK <maxravindu99@gmail.com>
2024-06-11 10:45:30 +0530

    updated

--- 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -9,6 +10,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -19,6 +21,8 @@ import javax.sql.DataSource;
  * @project Spring_Framework
  */
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
 public class JPAConfig {
 
     //Create BeanFactory
