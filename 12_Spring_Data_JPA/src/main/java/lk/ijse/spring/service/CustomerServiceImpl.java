736b08f94ba4e581c58d3ecd3da7a9dcf1ae5873
Ranger-HK <maxravindu99@gmail.com>
2024-06-11 17:12:35 +0530

    updated

---
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.entity.Customer;
+
+import java.util.List;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 6/11/2024 - 5:09 PM
+ * @project Spring_Framework
+ */
+public class CustomerServiceImpl {
+    public void saveCustomer(){}
+    public void deleteCustomer(){}
+    public void updateCustomer(){}
+    public Customer searchCustomer(){}
+    public List<Customer> getAllCustomer(){}
+
+
