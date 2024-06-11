0d6439d8cdf1542a2e7aae3a3443533f262a1175
Ranger-HK <maxravindu99@gmail.com>
2024-06-11 15:23:20 +0530

    updated

--- 12_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ 12_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -1,8 +1,12 @@
+package lk.ijse.spring.repo;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 6/11/2024 - 3:21 PM
  * @project Spring_Framework
- */class CustomerRepoTest {
-  
+ */
+class CustomerRepoTest {
+
 }
