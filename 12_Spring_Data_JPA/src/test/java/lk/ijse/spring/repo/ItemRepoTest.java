6d21164be57930fa43f1c1b298f9a268f1dbe149
Ranger-HK <maxravindu99@gmail.com>
2024-06-11 16:24:55 +0530

    updated

---
+++ 12_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/ItemRepoTest.java
@@ -0,0 +1,75 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.config.JPAConfig;
+import lk.ijse.spring.entity.Item;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 6/11/2024 - 4:16 PM
+ * @project Spring_Framework
+ */
+@WebAppConfiguration //State test configuration class
+@ContextConfiguration(classes = {JPAConfig.class}) //Import configurations for text context
+@ExtendWith(SpringExtension.class) //Run with Spring Extension
+class ItemRepoTest {
+
+    @Autowired
+    ItemRepo itemRepo;
+
+    @Test //Test Method
+    public void saveItem() {
+        //Ok lets save a item using Item Repo
+        Item item1 = new Item("I001", "Apple", 20, 100.0);
+        Item item2 = new Item("I002", "Mango", 30, 80.0);
+        Item item3 = new Item("I003", "Pineapple", 40, 120.0);
+
+        itemRepo.save(item1);
+        itemRepo.save(item2);
+        itemRepo.save(item3);
+
+    }
+
+
+    //get all items list
+    @Test
+    public void getAllItems() {
+        List<Item> all = itemRepo.findAll();
+        for (Item item : all) {
+            System.out.println(item.toString());
+        }
+    }
+
+    //Search Item
+    @Test
+    public void searchItem() {
+        Optional<Item> optional = itemRepo.findById("I001");
+        boolean present = optional.isPresent();
+        System.out.println(present);
+
+        Item item = optional.get();
+        System.out.println(item);
+    }
+
+    //Delete Item
+    @Test
+    public void deleteItem() {
+        itemRepo.deleteById("I003");
+    }
+
+    //Update Item
+    @Test
+    public void updateItem() {
+        Item item3 = new Item("I003", "Pineapple", 40, 120.0);
+        itemRepo.save(item3);
+    }
+
+}
