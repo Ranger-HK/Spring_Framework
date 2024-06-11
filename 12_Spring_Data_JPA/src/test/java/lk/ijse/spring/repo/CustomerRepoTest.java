e8b44cdd257b8a974db5da931c3cb8922f8c53cb
Ranger-HK <maxravindu99@gmail.com>
2024-06-11 15:47:23 +0530

    updated

--- 12_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ 12_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -1,5 +1,14 @@
 package lk.ijse.spring.repo;
 
+import lk.ijse.spring.config.JPAConfig;
+import lk.ijse.spring.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -7,6 +16,24 @@ import static org.junit.jupiter.api.Assertions.*;
  * @created 6/11/2024 - 3:21 PM
  * @project Spring_Framework
  */
+
+@WebAppConfiguration
+@ContextConfiguration(classes = {JPAConfig.class})
+@ExtendWith(SpringExtension.class)
 class CustomerRepoTest {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Test
+    public void saveCustomer(){
+       Customer customer1 = new Customer("C001", "Ravindu", "Bandaragama", 100000);
+       Customer customer2 = new Customer("C002", "Kamal", "Panadura", 20000);
+       Customer customer3 =new Customer("C003", "Nimal", "Colombo", 30000);
+
+       customerRepo.save(customer1);
+       customerRepo.save(customer2);
+       customerRepo.save(customer3);
+    }
 
 }
