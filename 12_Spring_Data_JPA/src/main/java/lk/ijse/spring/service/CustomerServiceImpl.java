35ecc2e65ee6aab1a7e617dd8a3ac32a50013589
Ranger-HK <maxravindu99@gmail.com>
2024-06-11 17:18:14 +0530

    updated

--- 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring.service;
 
 import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -10,26 +12,30 @@ import java.util.List;
  * @project Spring_Framework
  */
 public class CustomerServiceImpl {
-    public void saveCustomer(Customer entity) {
 
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    public void saveCustomer(Customer entity) {
+        customerRepo.save(entity);
     }
 
     public void deleteCustomer(String id) {
-
+        customerRepo.deleteById(id);
 
     }
 
     public void updateCustomer(Customer entity) {
-
+        customerRepo.save(entity);
 
     }
 
     public Customer searchCustomer(String id) {
-
+        return customerRepo.findById(id).get();
     }
 
     public List<Customer> getAllCustomer() {
-
+        return customerRepo.findAll();
     }
 }
 
