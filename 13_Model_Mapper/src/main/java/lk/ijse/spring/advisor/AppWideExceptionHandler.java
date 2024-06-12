bf0cf836bd9986f2c3d011efc3911c162e229d1e
Ranger-HK <maxravindu99@gmail.com>
2024-06-12 22:32:54 +0530

    updated

--- 13_Model_Mapper/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
+++ 13_Model_Mapper/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.advisor;
 
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,8 +14,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @CrossOrigin
 public class AppWideExceptionHandler {
 
-    @ExceptionHandler({Exception.class})
+   /* @ExceptionHandler({Exception.class})
     public String exceptionHandler(Exception e){
         return e.getMessage();
+    }*/
+
+    @ExceptionHandler({Exception.class})
+    public ResponseUtil exceptionHandler(Exception e){
+        return new ResponseUtil(500,e.getMessage(),null);
     }
 }
