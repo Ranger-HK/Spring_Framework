069d9603aac3d47e80ec2960189e91740a87c99f
Ranger-HK <maxravindu99@gmail.com>
2024-06-15 16:19:40 +0530

    Updated

--- 14_Spring_Boot/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ 14_Spring_Boot/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
